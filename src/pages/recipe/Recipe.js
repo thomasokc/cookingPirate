c827a8ddac9cfe5bb728a1103a325b6217234233
Tom Morris <thomasmorris682@gmail.com>
2022-04-12 20:44:33 -0500

    Added recipe info

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,9 +1,18 @@
+import {useParams} from 'react-router-dom'
+import { useFetch } from '../../hooks/useFetch'
+
 import './Recipe.css'
 
-export default function Recipe() {
+export default function Recipe({recipes}) {
+    const {id} = useParams()
+    const url = 'http://localhost:3000/recipes/' + id
+    const {error,isPending, data: recipe} = useFetch(url)
+
     return (
-        <div>
-            Recipe
+        <div className='recipe'>
+            {error && <p className='error'>{error}</p>}
+            {isPending && <p className='loading'>Loading...</p>}
+            {recipe && <h1>{recipe.title}</h1>}
         </div>
     )
 }
