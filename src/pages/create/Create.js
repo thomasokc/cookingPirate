ba6525eafbd36721c669bc105136770e82f39eb8
Tom Morris <thomasmorris682@gmail.com>
2022-04-13 19:34:55 -0500

    added forms

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,9 +1,48 @@
+import { useState } from 'react'
+
 import './Create.css'
 
 export default function Create() {
+    const [title, setTitle] = useState('')
+    const [method, setMethod] = useState('')
+    const [cookingTime, setCookingTime] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(title,method,cookingTime)
+    }
+
     return (
-        <div>
-            Create
+        <div className='create'>
+            <h2 className='page-title'>Add a New Recipe</h2>
+
+            <form onSubmit={handleSubmit}>
+
+                <label>
+                    <span>Recipe title:</span>
+                    <input type="text" onChange={(e) => setTitle(e.target.value)}
+                    value = {title}
+                    required
+                    />
+                </label>
+
+                <label>Recipe Method:</label>
+                <textarea onChange={(e) => setMethod(e.target.value)} 
+                value={method}
+                required
+                />
+
+                <label>
+                    <span>Cooking time (minutes):</span>
+                    <input type="number" onChange={(e) => setCookingTime(e.target.value)}
+                    value={cookingTime}
+                    required
+                    />
+                </label>
+
+                <button className='btn'>submit</button>
+
+            </form>
         </div>
     )
 }
