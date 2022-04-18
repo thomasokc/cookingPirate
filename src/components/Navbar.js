913ba6e58c9b6e172c82ecfc2507804763908f85
Thomas Morris <thomasmorris682@gmail.com>
2022-04-18 18:41:37 -0500

    use theme logic

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,17 @@
 import {Link} from 'react-router-dom'
+import {useTheme} from '../hooks/useTheme'
+
 
 //styles
 import './Navbar.css'
 import Searchbar from './Searchbar'
 
+
 export default function Navbar() {
+    const {color} = useTheme()
+
     return (
-        <div className='navbar'>
+        <div className='navbar' style={{background: color}}>
             <nav>
                 <Link to="/" className='brand'>
                     <h1>Cooking Pirate</h1>
