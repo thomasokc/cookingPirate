d9a05e6c0a4c9aea86d8341736164f237c368e68
Thomas Morris <thomasmorris682@gmail.com>
2022-04-19 15:52:18 -0500

    themes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './pages/home/Home'
 import Create from './pages/create/Create'
 import Search from './pages/search/Search';
 import Recipe from './pages/recipe/Recipe';
+import ThemeSelector from './components/ThemeSelector';
 
 //Styles
 import './App.css'
@@ -15,6 +16,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Navbar />
+        <ThemeSelector />
         <switch>
           <Route exact path="/">
             <Home />
--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,11 +1,33 @@
-import { createContext } from "react"
+import { createContext, useReducer } from "react"
 
 export const ThemeContext = createContext()
 
+const themeReducer = (state, action) => {
+    switch (action.type) {
+        case 'CHANGE_COLOR':
+            return {...state, color: action.payload}
+            case 'CHANGE_MODE':
+                return {...state, mode:action.payload}
+        default:
+            return state
+    }
+}
+
 export function ThemeProvider({children}) {
+    const [state, dispatch] = useReducer(themeReducer, {
+        color: '#58249c',
+        mode: 'dark'
+    })
+
+    const changeColor = (color) => {
+        dispatch({type: 'CHANGE_COLOR', payload: color})
+    }
+    const changeMode = (mode) => {
+        dispatch({type: 'CHANGE_MODE', payload: mode})
+    }
     
     return (
-        <ThemeContext.Provider value={{color:'blue'}}>
+        <ThemeContext.Provider value={{...state, changeColor, changeMode}}>
             {children}
         </ThemeContext.Provider>
     )
